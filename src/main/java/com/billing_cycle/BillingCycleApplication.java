baeecdaff6cce110f21ef37bfc4f0640bcfe6bad
Raju Gharti <raju.gharti00000@gmail.com>
2024-08-20 16:17:41 -0400

    initial setup

---
+++ src/main/java/com/billing_cycle/BillingCycleApplication.java
@@ -0,0 +1,13 @@
+package com.billing_cycle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillingCycleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BillingCycleApplication.class, args);
+    }
+
+}
